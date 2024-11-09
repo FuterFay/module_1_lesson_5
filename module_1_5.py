09a03e8d10914ccd8cdd453052d40079e7692010
FuterFay <amolodcov11@gmail.com>
2024-11-09 14:45:07 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -2,5 +2,5 @@ immutable_var = (1, 2, "text", True)
 print("Immutable tuple:", immutable_var)
 # immutable_var[0]= 100 # Строка кода не работает корректно, потому что, immutable_var имеет тиип данных "tuple" и явлеется не изменяемым
 mutable_list = [1, 2, "str", True]
-mutable_list[3] = "string"
+mutable_list[2] = "string"
 print("Mutable list;", mutable_list)
